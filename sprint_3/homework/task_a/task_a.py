4746ddc5e9c9c7e180e34a792f9ecbb58570bdd1
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2023-11-21 19:57:16 +0600

    Added solution for task_a.py

--- sprint_3/homework/task_a/task_a.py
+++ sprint_3/homework/task_a/task_a.py
@@ -1,5 +1,24 @@
 def solution(n: int) -> list[str]:
-    return []
+    stack = []
+    result = []
+
+    def backtrack(open_n, close_n):
+        if open_n == close_n == n:
+            result.append("".join(stack))
+            return
+
+        if open_n < n:
+            stack.append("(")
+            backtrack(open_n + 1, close_n)
+            stack.pop()
+
+        if close_n < open_n:
+            stack.append(")")
+            backtrack(open_n, close_n + 1)
+            stack.pop()
+
+    backtrack(0, 0)
+    return result
 
 
 def main():
